95fde033a9912eb669d1b71e0e6def2300977c1c
Aliang-SEU <huangzhiliang555@vip.qq.com>
2018-04-23 23:12:14 +0800

    commit

--- src/Data20180423/RemoveNthNodeFromEndOfList.java
+++ src/Data20180423/RemoveNthNodeFromEndOfList.java
@@ -15,7 +15,21 @@ import Data20180422.ListNode;
 public class RemoveNthNodeFromEndOfList {
 
     public ListNode removeNthFromEnd(ListNode head, int n){
-
+        if(head.next == null)
+            return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = head;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        ListNode pre = dummy;
+        while(fast != null){
+            pre = pre.next;
+            fast = fast.next;
+        }
+        pre.next = pre.next.next;
+        return dummy.next;
     }
 
 }
