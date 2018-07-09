916a2ad0847aeb6177e047640f7d3da0d9e252c7
Aliang-SEU <huangzhiliang555@vip.qq.com>
2018-07-09 13:32:00 +0800

    first commit

---
+++ src/Data20180709/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,22 @@
+package Data20180709;
+
+public class FindMinimuminRotatedSortedArray {
+
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int l = 0, r = nums.length - 1;
+        if(nums[l] > nums[r]){
+            while(l < r - 1){
+                int m = l + (r - l) / 2;
+                if(nums[l] < nums[m]){
+                    l = m;
+                }else{
+                    r = m;
+                }
+            }
+        }
+        return Math.min(nums[l], nums[r]);
+    }
+}
